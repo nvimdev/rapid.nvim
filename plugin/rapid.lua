233d1f638dca5c4484deae1813ad055fead86412
mathew <glephunter@gmail.com>
2023-08-06 20:43:51 +0800

    update

--- plugin/rapid.lua
+++ plugin/rapid.lua
@@ -6,10 +6,10 @@ vim.g.loaded_rapid = true
 
 local api = vim.api
 
-api.nvim_set_hl(0, 'RapidComplete', { bold = true, fg = 'orange' })
-api.nvim_set_hl(0, 'RapidTimeTaken', { bold = true, fg = 'violet' })
-api.nvim_set_hl(0, 'RapidFile', { bold = true, fg = 'blue' })
-api.nvim_set_hl(0, 'RapidTargetPos', { bold = true, fg = 'green' })
+api.nvim_set_hl(0, 'RapidComplete', { bold = true, fg = 'orange', default = true })
+api.nvim_set_hl(0, 'RapidTimeTaken', { bold = true, fg = 'violet', default = true })
+api.nvim_set_hl(0, 'RapidFile', { bold = true, fg = 'blue', default = true })
+api.nvim_set_hl(0, 'RapidTargetPos', { bold = true, fg = 'green', default = true })
 
 api.nvim_create_user_command('Rapid', function(args)
   require('rapid').compile(args)
